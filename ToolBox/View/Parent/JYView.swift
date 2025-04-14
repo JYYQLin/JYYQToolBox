c0c648dcfb2d89d327bca8bcb3641881d0b0d999
JYYQLin <92315746+JYYQLin@users.noreply.github.com>
2025-04-14 16:59:10 +0800

    修改代码

--- ToolBox/View/Parent/JYView.swift
+++ ToolBox/View/Parent/JYView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public class JYView: UIView {
+open class JYView: UIView {
     
     public lazy var pio_scale: CGFloat = 1 {
         didSet {
@@ -15,13 +15,13 @@ public class JYView: UIView {
         }
     }
     
-    override init(frame: CGRect = .zero) {
+    public override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         
         yq_add_subviews()
     }
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
